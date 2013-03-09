ae30d29f9ac523060fcb073eaac0857125575760
Matt Lerner <matt@walkscore.com>
2013-03-09 15:10:29 -0800

    minor test update

--- tests/geocoder_tests.py
+++ tests/geocoder_tests.py
@@ -2,4 +2,8 @@ from nose.tools import *
 from geopy import geocoders  
 
 def test_index():
+    g = geocoders.Google()
+    assert g
+    place, (lat, lng) = g.geocode("10900 Euclid Ave in Cleveland")  
+    print "%s: %.5f, %.5f" % (place, lat, lng)
     print "testing"
