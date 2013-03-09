ba699b77b58fe6f08d4a1e1f4242daf0cfbf2dad
Jesseko <jesse@walkscore.com>
2013-03-09 14:52:25 -0800

    jesse testing that git is working for me

--- realchange/urls.py
+++ realchange/urls.py
@@ -7,7 +7,6 @@ URLS = [
     # Basic URLs
     Route(r'/', handlers.HomeHandler, name='home'),
 
-
     # CRON URLs
     Route(r'/cron/sync/fm/', cron.SyncFilemakerHandler, name='sync-filemaker'),
 ]
